3de7d961616c3c722930121537bc69a43f063fce
elias <elias.aitboukha@gmail.com>
2023-10-03 00:31:33 +0200

    upd photo prophil style

--- public/assets/js/registrationForm/updatepp.js
+++ public/assets/js/registrationForm/updatepp.js
@@ -8,8 +8,13 @@ addEventListener("DOMContentLoaded", (event) => {
                 if(document.querySelector('div#inputPP span')) {
                     document.querySelector('div#inputPP span').remove();
                 }
-                document.querySelector('div#inputPP').style = "background: url("+base64Data+") no-repeat; background-size: contain;";
-
+                document.querySelector('div#inputPP').style = "filter: opacity(0);";
+                setTimeout(() => {
+                    document.querySelector('div#inputPP').style = "background: url("+base64Data+") no-repeat; background-size: contain; filter: opacity(0);";
+                }, 500);
+                setTimeout(() => {
+                    document.querySelector('div#inputPP').style = "background: url("+base64Data+") no-repeat; background-size: contain; filter: opacity(1);";
+                }, 700);
             };
             reader.readAsDataURL(input.files[0]);
         }
