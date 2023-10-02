dfb0806a602ec8a447142548c2c07844b3563780
megtaho <122614093+megtaho@users.noreply.github.com>
2023-10-02 10:22:09 +0200

    add offres panel

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(OffreRepository $offresrespo): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'offres'=> $offresrespo ->findAll(),
         ]);
     }
 }
