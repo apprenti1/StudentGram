a9c04572326f4bc260f2fefefc20e158d7982042
Nicolas <nicolas.cataluna@gmail.com>
2023-10-17 12:19:50 +0200

    upd : page profile
    avancement de la page profil

--- public/assets/js/comment/comment.js
+++ public/assets/js/comment/comment.js
@@ -1,11 +1,27 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    var commenttoggle = 0;
-    document.getElementById("viewCommentCard").addEventListener("click", ()=>{
-        if(commenttoggle == 0){
-            commenttoggle = 1;
-            
-        }
-    })
+console.log("hehe");
+var commenttoggle = 0;
+var card = document.getElementById("viewCard")
+function viewcard() {
+    console.log("test");
+    if (commenttoggle == 0) {
+        commenttoggle = 1;
+        card.style.height = "110px";
+        card.style.margin = "var(--bs-gutter-y)";
+        card.style.padding = "10px";
+        card.style.visibility = "visible";
+        setTimeout(() => {
+            card.style.filter = "opacity(1)";
+        }, 500);
+    }else{
+        commenttoggle = 0;
+        card.style.filter = "opacity(0)";
+        card.style.height = "0px";
+        card.style.margin = "0";
+        card.style.padding = "0";
+        setTimeout(()=>{
+            card.style.visibility="hidden"
+        }, 500);
+        
 
-
-});
+    }
+}
