3ef8c3dd896f7e59f4c4c1c3d6a337eeea7aa7a9
megtaho <122614093+megtaho@users.noreply.github.com>
2023-12-16 01:01:23 +0100

    fix duree evenement

--- src/Form/EvenementType.php
+++ src/Form/EvenementType.php
@@ -6,6 +6,9 @@ use App\Entity\Evenement;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Salle;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,23 +17,35 @@ class EvenementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
-            ->add('date', null, ['widget'=>'single_text'])
-            ->add('heure', null, ['hours'=>[18,19,20,21,22,23]])
-            ->add('duree')
-            ->add('salle', EntityType::class, [
+            ->add('nom', TextType::class)
+            ->add('description', TextType::class)
+            ->add('date', null, ['widget' => 'single_text'])
+            ->add('heure', null, ['hours' => [18, 19, 20, 21, 22, 23]])
+            ->add('duree', TimeType::class, [
+                'widget' => 'choice', // This option renders the time as a dropdown list
+                'label' => 'Durée',
+                'required' => true,
+            ]);
+        if ($options['is_admin']) {
+            $builder->add('salle', EntityType::class, [
                 'class' => Salle::class,
                 'choice_label' => 'nom',
-                'placeholder' => 'Select une salle',
-            ]);
+                'placeholder' => 'Selectionner une salle',
+            ])
+                ->add('valide', CheckboxType::class, [
+                    'label' => 'Valide',
+                'required' => false,
+                ]);
 
+
+        }
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Evenement::class,
+            'is_admin' => false,
         ]);
+        $resolver->setAllowedTypes('is_admin', 'bool');
     }
 }
